6c6b44e93693bbe49ad5abff39b6e0c56c52e7ac
yuki153 <37236474+yuki153@users.noreply.github.com>
2024-01-16 12:43:11 +0900

    add: sass(scss) & happy-css-module

---
+++ src/styles/shared.module.scss.d.ts
@@ -0,0 +1,5 @@
+declare const styles:
+  & Readonly<{ "box": string }>
+;
+export default styles;
+//# sourceMappingURL=./shared.module.scss.d.ts.map
---
+++ src/styles/test.module.scss.d.ts
@@ -0,0 +1,7 @@
+declare const styles:
+  & Readonly<Pick<(typeof import("./shared.module.scss"))["default"], "box">>
+  & Readonly<{ "pageTitle": string }>
+  & Readonly<{ "headline": string }>
+;
+export default styles;
+//# sourceMappingURL=./test.module.scss.d.ts.map
